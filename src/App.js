9fafb8cce103cffc9c2637c9ca2fcfc27e597d39
Holly <hollyduckworth@hotmail.co.uk>
2020-05-02 12:40:39 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TaskItem from "./TaskItem/TaskItem"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Hello Holly!
+        </h1>
       </header>
+
+      <TaskItem text="feed Jeff"/>
+      <TaskItem text ="drink Wine"/>
+      <TaskItem text ="wash Alan"/>
+      <TaskItem text ="feed children"/>
+      <TaskItem text = "be fabulous"/>
+
+      <h4>Nash on then you Radgee!!</h4>
     </div>
   );
 }
---
+++ src/TaskItem/TaskItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './TaskItem.css';
+
+function TaskItem(props) {
+    return (
+        <div className="task-item">
+            <p className="task-item_text">{ props.text }</p>
+            <button className ="task-item_done-button">Done</button>
+            <button className = "task-item_delete-button"> Delete</button>
+        </div>
+    );
+}
+
+export default TaskItem;
+
+// props = {
+//     text: "Feed Jeff"
+// }
