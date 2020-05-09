b708ec2d7b86fa279dc10a367f36b2bc54ed72f3
Holly <hollyduckworth@hotmail.co.uk>
2020-05-09 10:12:16 +0100

    handing in for now

--- src/headerToggle/HeaderToggle.js
+++ src/headerToggle/HeaderToggle.js
@@ -3,7 +3,7 @@ import './HeaderToggle.css';
 
 function HeaderToggle(props) {
     return (
-        <div className="header-toggle">
+        <div className="container header-toggle">
             <div className="row header-toggle__row">
                 <div className="btn-group btn-group-toggle" data-toggle="buttons">
                     <label className="btn btn-secondary active">
