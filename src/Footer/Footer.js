97b088e4f298948aa613b819811802c06cecc4a5
Holly <hollyduckworth@hotmail.co.uk>
2020-05-10 18:28:35 +0100

    trying to get the footer back

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -4,10 +4,8 @@ import './Footer.css';
 function Footer() {
     return (
         <footer className="text-center">
-            <p><a href="headerToggle/HeaderToggle.js"
-                // class="btn btn-primary btn-sm active"
-                // role="button" aria-pressed="true">
-                >Nip back to the top of the page</a></p>
+                <p><a href="Header/Header.js">
+                    Nip back to the top of the page</a></p>
         </footer>
 
     );
@@ -15,3 +13,5 @@ function Footer() {
 
 
 export default Footer;
+
+{/* <a href="https://<user.name>.github.io/<repo.name>/about.html" title="About Me">About Me</a> */}
