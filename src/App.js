dc85765d70b3e05fc0d632ab2d3b2f7115c842b1
Holly <hollyduckworth@hotmail.co.uk>
2020-06-20 23:46:46 +0100

    get and post now working and linke dto back end and front end but dates are not showing up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import React, { useState, useEffect, Fragment } from 'react';
+// import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
 
 import Header from "./Header/header"
 import TaskItem from "./TaskItem/TaskItem"
@@ -15,29 +16,28 @@ import Footer from "./Footer/Footer"
 function App() {
   // const tasks = useState() []; //what is that state
   // const setTasks =useState()[1]; //function allows me to update that state
-  //Destructuring method, first thing we will store
+  // const [tasks, setTasks] = useState([
+  //   { text: "Look up isBefore and momentjs.com", completed: false, dueDate: "2020-05-20", priority: "high", id: uuidv4() },
+  //   { text: "try and order you lists by date", completed: false, dueDate: "2020-05-20", priority: "high", id: uuidv4() },
+  // ]);
 
-  const [tasks, setTasks] = useState([
-    { text: "Look up isBefore and momentjs.com", completed: false, dueDate: "2020-05-20", priority: "high", id: uuidv4() },
-    { text: "try and order you lists by date", completed: false, dueDate: "2020-05-20", priority: "high", id: uuidv4() },
-    { text: "Wash Alan", completed: false, dueDate: "2020-05-17", priority: "high", id: uuidv4() },
-    { text: "Find the cat", completed: false, dueDate: "2020-05-17", priority: "medium", id: uuidv4() },
-    { text: "Take a secret nap", completed: false, dueDate: "2020-05-24", priority: "low", id: uuidv4() },
-    { text: "Make a white Russian", completed: true, dueDate: "2020-05-01", priority: "doneColor", id: uuidv4() },
-    { text: "Feed children", completed: true, dueDate: "2020-06-07", priority: "doneColor", id: uuidv4() },
-    { text: "Shave little shorts into Jeffery the cat", completed: false, dueDate: "2020-07-07", priority: "low", id: uuidv4() },
-    { text: "Eat Cheese", completed: true, dueDate: "2020-06-07", priority: "doneColor", id: uuidv4() },
-    { text: "Remove and cut up the entire back garden hedge", completed: true, dueDate: "2020-06-07", priority: "doneColor", id: uuidv4() },
-    { text: "Get into bin", completed: true, dueDate: "2020-06-07", priority: "doneColor", id: uuidv4() },
-    { text: "Buy a reciprocal saw", completed: false, dueDate: "2020-04-29", priority: "medium", id: uuidv4() },
-    { text: "Paint woodwork", completed: false, dueDate: "2020-07-29", priority: "low", id: uuidv4() }
-  ]);
-
-  //trying to get dates to be sorted in order has to be here else it does not sort the original state above.
-  tasks.sort(function(a,b){
+  const [tasks, setTasks] = useState();
+  useEffect(() => {
+    axios.get('https://djlfzi1od5.execute-api.eu-west-2.amazonaws.com/dev/tasks')
+      .then(
+        response => {
+          console.log(response.data);
+          setTasks(response.data.tasks);
+        })
+      .catch(
+        (error) => {
+          console.log("error fetch data this is the error that request has given", error)
+        })
+  }, []);
+  // //trying to get dates to be sorted in order has to be here else it does not sort the original state above.
+  tasks && tasks.sort(function (a, b) {
     return new Date(a.dueDate) - new Date(b.dueDate);
   });
-
   //making a variable that tells us the date of the last day of THIS week
   let today = new Date(); //this is todays date and time
   let first = today.getDate() - today.getDay() + 1; // First day is the day of the month - the day of the week
@@ -56,111 +56,121 @@ function App() {
   let yyyyNW = lastdayNW.getFullYear();
   let lastDayOfWeekNW = yyyyNW + "-" + mmNW + "-" + ddNW;
 
-
-  const twTasks = tasks.filter(task => !task.completed && task.dueDate <= lastDayOfWeek);
+  const twTasks = tasks && tasks.filter(task => !task.completed && task.currentDueDate <= lastDayOfWeek);
   // this weeks tasks so looking at if NOT DONE and in a certain date range
-
-  const nwTasks = tasks.filter(task => !task.completed && task.dueDate > lastDayOfWeek
-    && task.dueDate <= lastDayOfWeekNW);
+  const nwTasks = tasks && tasks.filter(task => !task.completed && task.currentDueDate > lastDayOfWeek
+    && task.currentDueDate <= lastDayOfWeekNW);
   // this weeks tasks so looking at if NOT DONE and in a certain date range
-
-  const allActiveTasks = tasks.filter(task => !task.completed);
+  const allActiveTasks = tasks && tasks.filter(task => !task.completed);
   //look through array of ALLTASKS and if the task if false keep it
-
-  const doneTasks = tasks.filter(task => task.completed);
+  const doneTasks = tasks && tasks.filter(task => task.completed);
 
   //function to delete a task
   function deleteTask(id) {
-    //look through all tasks and find where task.id===id
-    //if it IS === remove that task
-    const updatedTasks = tasks.filter(task => task.id !== id); //if it return TRUE it keeps it, if false it removes it
+    //look through all tasks and find where task.todoId===id if it IS === remove that task
+    const updatedTasks = tasks && tasks.filter(task => task.todoId !== id); //if it return TRUE it keeps it, if false it removes it
     //then update TASK STATE
     setTasks(updatedTasks);
   }
 
+  // axios.delete('https://djlfzi1od5.execute-api.eu-west-2.amazonaws.com/dev/tasks/20')
+  //   .then(response => {
+  //       console.log(response.data);
+  //       setTasks(response.data.tasks);
+  //     })
+  //   .catch(error => {
+  //       console.log("can't delete the task", error)
+  //     })
+
   function completeTask(id) {
-    //look through all tasks and find where task.id===id, 
+    //look through all tasks and find where task.todoId===id, 
     //if it is change completed: true
     //if it IS then add to updated task state
-    const updatedTasks = tasks.map(task => {
-      if (task.id === id) {
+    const updatedTasks = tasks && tasks.map(task => {
+      if (task.todoId === id) {
         task.completed = true;
-      }
-      return task;
+      } return task;
     })
-
     setTasks(updatedTasks);
   }
 
   function addTask(text, dueDate, priority) {
-    //get a copy of the tasks that are akready there
+    //get a copy of the tasks that are aready there
     //add a new task into this array
     //update the task state
-    const newTask = { 
-      text: text, 
-      completed: false, 
-      dueDate: dueDate, 
-      priority: priority, 
-      id: uuidv4() 
+    const newTask = {
+      text: text,
+      completed: false,
+      currentDueDate: dueDate,
+      priority: priority,
+      // todoId: uuidv4()
     }
-    //using a spread syntax to go through wach task and the new tasks
-    const updatedTasks = [...tasks, newTask]
-    setTasks(updatedTasks);
+
+    axios
+      .post('https://djlfzi1od5.execute-api.eu-west-2.amazonaws.com/dev/tasks', newTask)
+      .then(
+        //If the request is successful, get the task id and add it to the new task object
+        (response) => {
+          newTask.todoId = response.data.task[0].todoId;
+          console.log(newTask);
+          const updatedTasks = [...tasks, newTask];
+          setTasks(updatedTasks);
+          console.log(tasks);
+        }
+      )
+      .catch(error => {
+        console.log('Error adding a task', error)
+      })
   }
 
-  
-
-
-
- 
   return (
     <div className="Container">
       <div className="App">
-        <Header addTask={ addTask }/>
+        <Header addTask={addTask} />
         <br></br>
         <ThisWeekH />
         <div>
-          {twTasks.map((tasks) => {
+          {twTasks && twTasks.map((tasks) => {
             return <TaskItem
               deleteTask={deleteTask}
               completeTask={completeTask}
               key={tasks.text}
               text={tasks.text}
-              dueDate={tasks.dueDate}
+              dueDate={tasks.currentDueDate}
               completed={tasks.completed}
               priority={tasks.priority}
-              id={tasks.id} />
+              id={tasks.todoId} />
           })}
         </div>
         <br></br>
         <br></br>
         <h3>NEXT WEEK's to-do's</h3>
         <div>
-          {nwTasks.map((tasks) => {
+          {nwTasks && nwTasks.map((tasks) => {
             return <NextWeek
               deleteTask={deleteTask}
               completeTask={completeTask}
               key={tasks.text}
               text={tasks.text}
-              dueDate={tasks.dueDate}
+              dueDate={tasks.currentDueDate}
               completed={tasks.completed}
               priority={tasks.priority}
-              id={tasks.id} />
+              id={tasks.todoId} />
           })}
         </div>
         <br></br>
         <br></br>
         <AllTodoH />
-        {allActiveTasks.map((tasks) => {
+        {allActiveTasks && allActiveTasks.map((tasks) => {
           return <AllTodo
             deleteTask={deleteTask}
             completeTask={completeTask}
             key={tasks.text}
             text={tasks.text}
-            dueDate={tasks.dueDate}
+            dueDate={tasks.currentDueDate}
             completed={tasks.completed}
             priority={tasks.priority}
-            id={tasks.id} />
+            id={tasks.todoId} />
         })}
         <div>
         </div>
@@ -168,14 +178,14 @@ function App() {
         <br></br>
         <DoneH />
         <div>
-          {doneTasks.map((tasks) => {
+          {doneTasks && doneTasks.map((tasks) => {
             return <Done
               deleteTask={deleteTask}
               key={tasks.text}
               text={tasks.text}
               completed={tasks.completed}
               priority={tasks.priority}
-              id={tasks.id} />
+              id={tasks.todoId} />
           })}
         </div>
         <br></br>
@@ -183,8 +193,6 @@ function App() {
         <Footer />
       </div>
     </div>
-
-
 
   );
 }
--- src/TaskItem/TaskItem.js
+++ src/TaskItem/TaskItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 //fragemnt is an invisable div
 import './TaskItem.css';
 
@@ -9,6 +9,9 @@ function TaskItem(props) {
         props.completeTask(props.id);
     }
 
+    const [dueDate, setDueDate] = useState("");
+    function handleDateChange(event) {setDueDate(event.target.value);}
+
     return (
         <div className={`task-item ${props.priority}`}>
             <div className="row">
@@ -16,18 +19,30 @@ function TaskItem(props) {
                     <p>{props.text}</p>
                 </div>
                 <div className="col-12 col-md-2 dateBox">
-                    <input id="due-date" type="date" className="form-control" defaultValue={props.dueDate} />
+                    <input 
+                    id="due-date" 
+                    type="date" 
+                    className="form-control" 
+                    defaultValue={props.currentDueDate}
+                    />
                 </div>
                 <div className="col-12 col-md-2 doneDeleteButtons">
+                <button
+                        type="button"
+                        className="btn btn-outline-dark"
+                        defaultValue={props.currentDueDate}
+                        onChange={handleDateChange}
+                        > change date </button>
                     {!props.completed &&
                         <button
                             type="button"
-                            className="btn btn-outline-dark"
+                            className="btn btn-dark"
                             onClick={handleDoneClick}> done</button>}
                     <button
                         type="button"
-                        className="btn btn-outline-dark"
+                        className="btn btn-dark"
                         onClick={() => props.deleteTask(props.id)}> delete </button>
+                   
                 </div>
             </div>
         </div >
